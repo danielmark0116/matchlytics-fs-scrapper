50929f8ac726761968ec0950500e141a752fa820
Daniel Grychtoł <grychtol.daniel@gmail.com>
2020-11-01 17:13:11 +0100

    Protect scrapper initialization route

--- server/src/routes/scrapper.routes.ts
+++ server/src/routes/scrapper.routes.ts
@@ -1,9 +1,16 @@
 import * as Express from "express";
 import * as ScrapperController from "../controllers/scrapper.controller";
+import * as passport from "passport";
+import { adminMiddleware } from "../middlewares/admin.middleware";
 
 const router = Express.Router();
 
-router.get("/", ScrapperController.startScraping);
+router.get(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  adminMiddleware,
+  ScrapperController.startScraping
+);
 router.get("/analysis", ScrapperController.getLatestAnalysis);
 
 export default router;
