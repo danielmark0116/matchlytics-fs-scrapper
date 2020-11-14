54c3c9415dd574ad449b961bbb131186343107fb
Daniel Grychtoł <grychtol.daniel@gmail.com>
2020-11-14 22:54:25 +0100

    Add roles management

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,6 +1,7 @@
 import * as Express from "express";
 import * as UsersController from "../controllers/users.controller";
 import * as passport from "passport";
+import { adminMiddleware } from "../middlewares/admin.middleware";
 
 const router = Express.Router();
 
@@ -9,6 +10,17 @@ router.get(
   passport.authenticate("jwt", { session: false }),
   UsersController.getUser
 );
-router.get("/create", UsersController.createUser);
+router.get(
+  "/all",
+  passport.authenticate("jwt", { session: false }),
+  adminMiddleware,
+  UsersController.getUsers
+);
+router.patch(
+  "/:user_id/role/:role",
+  passport.authenticate("jwt", { session: false }),
+  adminMiddleware,
+  UsersController.changeRole
+);
 
 export default router;
